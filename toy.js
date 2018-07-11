6bd1d390a4be9d16fd639c8de9ddcdac06afb0ba
jlhao <807846356@qq.com>
2018-07-11 15:10:08 +0800

    æ·»å åå®¹

--- toy.js
+++ toy.js
@@ -65,9 +65,24 @@ var place = function(state){
 
 var move = function(){
     if (!toy.face) return;
+	if (toy.face === "WEST"){
+		var next = toy.x - 1;
+		if (next >= 0) toy.x = next;
+     }
+	if (toy.face === "NORTH"){
+        var next = toy.y + 1;
+		if (next <= 5) toy.y = next;
+     }
+	 if (toy.face === "EAST"){
+        var next = toy.x + 1;
+		if (next <= 5) toy.x = next;
+     }
+	 if (toy.face === "SOUTH"){
+        var next = toy.y - 1;
+		if (next >= 0) toy.y = next;
+     }
     
-    var next = toy.y + 1;
-    if (next <= 5) toy.y = next;
+
 };
 
 var left = function(){
